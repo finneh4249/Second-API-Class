88c4487e0a1beae381ca3b8563919c2c634db77c
Ethan Cornwill <ethancornwill@outlook.com>
2024-08-22 20:56:30 +1000

    Add card entries into cli command "seed"

--- controllers/cli_controllers.py
+++ controllers/cli_controllers.py
@@ -2,7 +2,9 @@ from flask import Blueprint
 from init import db, bcrypt
 
 from models.user import User
+from models.card import Card
 
+from datetime import date
 db_commands = Blueprint("db", __name__)
 
 @db_commands.cli.command("create")
@@ -30,7 +32,36 @@ def seed_db():
             password=bcrypt.generate_password_hash("user").decode("utf-8"), 
             )
         ]
-        
+    
     db.session.add_all(users)
+
+    cards = [
+        Card(
+            title = "Github Operations",
+            description = "Perform mandatory github ops on the project",
+            status = "To Do",
+            priority = "High",
+            date = date.today(),
+            user = users[0]
+        ), 
+        Card(
+            title = "Initialise the modules",
+            description = "Perform init operations on the necessary modules",
+            status = "Ongoing",
+            priority = "High",
+            date = date.today(),
+            user = users[0]            
+        ), 
+        Card(
+            title = "Add comments to code",
+            description = "Add meaningful comments when necessary",
+            status = "To Do",
+            priority = "High",
+            date = date.today(),
+            user = users[1]
+            )
+        ]
+        
+    db.session.add_all(cards)
     db.session.commit()
     print("Database seeded")

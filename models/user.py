3b55031d54a6c807ba6319cc108007053c7c1f6d
Ethan Cornwill <ethancornwill@outlook.com>
2024-08-24 10:20:42 +1000

    add comments to all code to help classmates understand what's going on

--- models/user.py
+++ models/user.py
@@ -2,6 +2,16 @@ from init import db, ma
 from marshmallow import fields
 
 class User(db.Model):
+    """
+    This class represents the User model in the database
+
+    Columns:
+    - id: The primary key of the user
+    - name: The name of the user
+    - email: The email of the user
+    - password: The password of the user
+    - is_admin: Whether the user is an admin or not
+    """
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
@@ -9,6 +19,10 @@ class User(db.Model):
     password = db.Column(db.String(255), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
 
+    """
+    This property represents the relationship between a Card and its User
+    The back_populates parameter is used to build the relationship between a Card and a User
+    """
     cards = db.relationship("Card", back_populates="user")
     
 class UserSchema(ma.Schema): 
